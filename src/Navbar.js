2ccf762fce83369130555a73f4e4fc50253a5a39
Mahmoudawd4 <mahmoudawd54@gmail.com>
2023-04-20 17:46:02 +0200

    add

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ function NavbarComponent() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">React Redux Router App</Link>
+        <Link className="navbar-brand" to="/">lab3</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
